0b860818d5c526bcdbf7eb0304a2cfe1be41a9b9
Christopher Scott Tadlock <christopher.s.tadlock@gmail.com>
2019-04-10 16:29:13 -0400

    Add files via upload

---
+++ BowCategorySelection.java
@@ -0,0 +1,13 @@
+package com.tadlockc.ffbearmory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BowCategorySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bow_category_selection);
+    }
+}
---
+++ HammersCategorySelection.java
@@ -0,0 +1,13 @@
+package com.tadlockc.ffbearmory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HammersCategorySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hammers_category_selection);
+    }
+}
